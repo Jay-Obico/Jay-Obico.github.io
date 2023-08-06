ab6cd6c0ba4cb079b905917b25827cefbdb4fc2e
Jay-Obico <83934873+Jay-Obico@users.noreply.github.com>
2023-08-06 10:41:55 +0800

    Added Template for Conferences, Awards and Achievements, Thesis Archive.
    Updated JavaScript and CSS, Updated Navbar

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 window.onload = (function(){
     try{
         document.getElementById("ECE-btn").disabled = true;
-        document.getElementById("CpE").classList.add("d-none");
+        
     }catch(e){}
 
     try{
@@ -9,6 +9,10 @@ window.onload = (function(){
         document.getElementById("CpE").classList.add("d-none");
     }catch(e){}
     
+    try{
+        document.getElementById("Faculty-btn").disabled = true;
+        document.getElementById("Student_Achievement").classList.add("d-none");
+    }catch(e){}
 });
 
 function getID(clicked){
@@ -18,8 +22,8 @@ function getID(clicked){
 
     if(instance==="ECE-btn"){
 
-        document.getElementById("ECE").classList.remove("d-none");
-        document.getElementById("CpE").classList.add("d-none");
+        document.getElementById("ECE").style.display = 'block';
+        document.getElementById("CpE").style.display = 'none';
         document.getElementById("ECE-btn").disabled = true;
         document.getElementById("CpE-btn").disabled = false;
     
@@ -27,8 +31,8 @@ function getID(clicked){
     }
     else if(instance==="CpE-btn"){
         
-        document.getElementById("ECE").classList.add("d-none");
-        document.getElementById("CpE").classList.remove("d-none");
+        document.getElementById("ECE").style.display = 'none';
+        document.getElementById("CpE").style.display = 'block';
         document.getElementById("CpE-btn").disabled = true;
         document.getElementById("ECE-btn").disabled = false;
         
@@ -41,13 +45,46 @@ function getID(clicked){
         document.getElementById("CpE-btn-CSO").disabled = false;
 
     }else if (instance==="CpE-btn-CSO"){
+
         document.getElementById("ECE").classList.add("d-none");
         document.getElementById("CpE").classList.remove("d-none");
         document.getElementById("CpE-btn-CSO").disabled = true;
         document.getElementById("ECE-btn-CSO").disabled = false;
 
+    }else if (instance==="Faculty-btn"){
+
+        document.getElementById("Student_Achievement").classList.add("d-none");
+        document.getElementById("Faculty_Achievement").classList.remove("d-none");
+        document.getElementById("Faculty-btn").disabled = true;
+        document.getElementById("Student-btn").disabled = false;
+
+    }else if (instance==="Student-btn"){
+
+        document.getElementById("Faculty_Achievement").classList.add("d-none");
+        document.getElementById("Student_Achievement").classList.remove("d-none");
+        document.getElementById("Student-btn").disabled = true;
+        document.getElementById("Faculty-btn").disabled = false;
+
     }
  }
 
+function on_Click_Disable(clicked){
+
+    var childrenList = document.getElementById("Thesis-btn-group").children;
+    
+    // document.getElementById(clicked.id).disabled=true;
+    for(var i=0; i <childrenList.length; i++){
+        console.log(clicked.textContent === childrenList[i].textContent);
+        
+        if(clicked.textContent === childrenList[i].textContent){
+            childrenList[i].disabled=true;
+            
+        }else{
+            childrenList[i].disabled=false;
+        }
+    }
+
+
+}
 
  
